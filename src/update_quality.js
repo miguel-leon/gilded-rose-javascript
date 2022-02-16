bf278d4e7c17c5a0c9b21c95916c9c450ac60b62
Miguel Leon <miguel.e.leon.f@gmail.com>
2022-02-16 19:09:40 -0300

    🎈 pass test "lowers quality twice as fast for "Conjured" items".

--- src/update_quality.js
+++ src/update_quality.js
@@ -1,5 +1,5 @@
 
-function update_quality(items) {
+function update_quality(items, pass_again = true) {
 	for (var i = 0; i < items.length; i++) {
 		if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
 			if (items[i].quality > 0) {
@@ -43,6 +43,11 @@ function update_quality(items) {
 					items[i].quality = items[i].quality + 1
 				}
 			}
+		}
+
+		if (items[i].name === 'Conjured' && pass_again) {
+			items[i].sell_in++;
+			items[i] = update_quality([items[i]], false)[0];
 		}
 	}
 
