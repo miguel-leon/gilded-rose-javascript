e66decd5762d272b757b4caf31ed16963167e3eb
Miguel Leon <miguel.e.leon.f@gmail.com>
2022-02-16 18:46:03 -0300

    🧪 add test for "lowers both values for every item".

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,21 @@
-describe("Gilded Rose", function() {
+const Item = require('../src/item');
+const update_quality = require('../src/update_quality');
 
-  it("should do something", function() {
-    update_quality();
-  });
+
+describe("Gilded Rose", function () {
+
+	it("should lowers both values for every item", function () {
+		const items = [
+			new Item('+5 Dexterity Vest', 10, 20),
+			new Item('Elixir of the Mongoose', 5, 7)
+		];
+
+		const result = update_quality(items);
+
+		expect(result).toEqual([
+			new Item('+5 Dexterity Vest', 9, 19),
+			new Item('Elixir of the Mongoose', 4, 6)
+		]);
+	});
 
 });
