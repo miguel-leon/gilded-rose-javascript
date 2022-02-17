584f594ae1f61c45aeb9b1f55f59df58740a59fa
Miguel Leon <miguel.e.leon.f@gmail.com>
2022-02-16 23:58:55 -0300

    🧪 add test for "perform correctly for all original items".

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -118,4 +118,26 @@ describe('Gilded Rose', function () {
 		]);
 	});
 
+	it('should perform correctly for all original items', function () {
+		const items = [
+			new Item('+5 Dexterity Vest', 10, 20),
+			new Item('Aged Brie', 2, 0),
+			new Item('Elixir of the Mongoose', 5, 7),
+			new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+			new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+			new Item('Conjured Mana Cake', 3, 6)
+		];
+
+		const result = update_quality(items);
+
+		expect(result).toEqual([
+			new Item('+5 Dexterity Vest', 9, 19),
+			new Item('Aged Brie', 1, 1),
+			new Item('Elixir of the Mongoose', 4, 6),
+			new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+			new Item('Backstage passes to a TAFKAL80ETC concert', 14, 21),
+			new Item('Conjured Mana Cake', 2, 4)
+		]);
+	});
+
 });
